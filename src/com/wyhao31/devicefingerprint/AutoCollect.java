feb5f258ca16db31d9749cf23f6227af47157456
wyhao31 <wyhao531@gmail.com>
2014-05-20 15:27:03 +0800

    wake lock bug fix

--- src/com/wyhao31/devicefingerprint/AutoCollect.java
+++ src/com/wyhao31/devicefingerprint/AutoCollect.java
@@ -1,15 +1,10 @@
 package com.wyhao31.devicefingerprint;
 
-import com.wyhao31.devicefingerprint.db.DBManager;
-
-import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.PowerManager;
 import android.util.Log;
 
-@SuppressLint("Wakelock")
 public class AutoCollect extends BroadcastReceiver {
 
 	@Override
